9acee76c58c04cf29f342f993ed6d505f169a780
vishaldewani206 <vishaldewani500@gmail.com>
2026-08-25 15:15:24 +0500

    add toArray, equals and toStringReverse methods in LinkedQueue

--- src/queue/LinkedQueue.java
+++ src/queue/LinkedQueue.java
@@ -73,9 +73,86 @@ public class LinkedQueue implements Queue{
 
     }
 
+    public String toStringReverse(){
+        if(isEmpty()) throw new IllegalStateException("Queue is empty");
+
+        StringBuilder sb = new StringBuilder();
+        Node temp = head;
+
+        while(temp.prev != head){
+            sb.append(temp.prev.data).append(" ");
+            temp = temp.prev;
+        }
+
+        return sb.toString();
+    }
+
+    public int[] toArray(int n){
+        if(isEmpty()) throw new IllegalStateException("Queue is empty");
+
+        int[] arr = new int[n];
+
+        Node temp = head.prev;
+        int i = 0;
+        while(temp != head){
+            if(i == n){
+                break;
+            }
+            arr[i] = (int) temp.data;
+            i++;
+            temp = temp.prev;
+        }
+
+        return arr;
+
+    }
+
+    public int[] toArray(int start, int end){
+        if (start < 0 || end >= size || start > end) {
+            throw new IndexOutOfBoundsException("Incorrect indices");
+        }
+
+        int[] arr = new int[end-start + 1];
+        Node temp = head.next;
+
+        for(int i = 0; i < start; i++){
+            temp = temp.next;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) temp.data;
+            temp = temp.next;
+        }
+
+        return arr;
+
+    }
+
+    public boolean equals(LinkedQueue other){
+        if(getSize() != other.getSize()){
+            return false;
+        }
+
+        Node temp = head;
+        Node temp2 = other.head;
+
+        while(temp.next != head && temp2.next != head){
+
+            if(temp.data != temp2.data){
+                return false;
+            }
+
+            temp = temp.next;
+            temp2 = temp2.next;
+        }
+
+        return true;
+
+
+    }
+
     public static void main(String[] args) {
         LinkedQueue linkedQueue = new LinkedQueue();
-
         linkedQueue.add(1);
         linkedQueue.add(8);
         linkedQueue.add(10);
@@ -87,6 +164,25 @@ public class LinkedQueue implements Queue{
         System.out.println("Queue after remove: " + linkedQueue);
         System.out.println("Queue size: " + linkedQueue.getSize());
         System.out.println("Queue first value: " + linkedQueue.first());
+//        System.out.println(linkedQueue.toStringReverse());
+
+
+        int[] arr = linkedQueue.toArray(2); // only ending: from 0 to n
+        int[] arr2 = linkedQueue.toArray(1, 3); // starting and ending
+
+
+        for (int j : arr2){
+            System.out.println("elem: " + j);
+        }
+
+
+        LinkedQueue linkedQueue1 = new LinkedQueue();
+        linkedQueue1.add(8);
+        linkedQueue1.add(10);
+        linkedQueue1.add(0);
+        linkedQueue1.add(5);
+
+        System.out.println("first queue = second queue: " +linkedQueue.equals(linkedQueue1));
 
     }
 
